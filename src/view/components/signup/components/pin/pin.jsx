4378c19c58239856f92f2c2b87c198ab7188828e
prashantjha <prashantjha@pop-os.localdomain>
2022-10-22 19:35:08 +0530

    refactor

--- src/view/components/signup/components/pin/pin.jsx
+++ src/view/components/signup/components/pin/pin.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import OtpInput from "react-otp-input";
 import { Typography } from "antd";
 import { CButton } from "../../../../shared/c-button";
